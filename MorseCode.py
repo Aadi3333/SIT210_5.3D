a8a49a496f2b3fabb283ee0cbf0a39b883075bc3
Aadi3333 <103951593+Aadi3333@users.noreply.github.com>
2022-10-31 18:34:30 +0530

    Update MorseCode.py

--- MorseCode.py
+++ MorseCode.py
@@ -1,24 +1,23 @@
 from multiprocessing.sharedctypes import Value
 from tkinter import *
 import tkinter.font
-from gpizero import LED
+from gpiozero import LED
 import RPi.GPIO as GPIO
 import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(11,GPIO.OUT)
 GPIO.setup(12,GPIO.OUT)
-GPIO.setup(14,GPIO.OUT)
+
 
 window = Tk()
-window.title("Morse Code Blinking Name")
+window.title("Morse Code")
 myFont = tkinter.font.Font(family="Helvetica", size=12, weight="bold")
 
 def dot():
-    GPIO.output(11,GPIO.HIGH)
+    GPIO.output(12,GPIO.HIGH)
     time.sleep(1)
-    GPIO.output(11,GPIO.LOW)
+    GPIO.output(12,GPIO.LOW)
     time.sleep(1)
     
 def dash():
@@ -39,34 +38,22 @@ Morse_Code= { 'A':'.-', 'B':'-...','C':'-.-.','D':'-..', 'E':'.',
 
 def Change_to_MorseCode():
     word =user_input.get()
-    print(" " + word)
-    for letter in word:
-        for symbol in Morse_Code[letter.upper()]:
-            if symbol=='.':
-                dot()
-            elif symbol=='-':
-                dash()
-            else:
-                time.sleep(0.5)
-
+    print(word)
     for element in word:
-        for symbol in Morse_Code[element]:
+        for symbol in Morse_Code[element.upper()]:
             if symbol=='.':
                 dot()
             elif symbol=='-':
                 dash()
             else:
                 time.sleep(0.5)
-
+                
     print("The name you have entered is blinked !")
 
-    GPIO.output(14,GPIO.HIGH)
-    time.sleep(1.50)
-    GPIO.output(14,GPIO.LOW)
-    time.sleep(1.50)
             
-user_input=Entry(window,text= 'MorseCode',font=myFont,width=25,bg='white')
+user_input=Entry(window,font=myFont,width=100,bg='grey')
 user_input.grid(row=0,column=0)
 
-button1=Button(window,text='Change',font=myFont,command=Change_to_MorseCode,bg='black',height=5,width=7)
+button1=Button(window,text='Change',font=myFont,command=Change_to_MorseCode,bg='blue',height=2,width=10)
 button1.grid(row=1,column=0)
+
